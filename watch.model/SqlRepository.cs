cd0e2808b5e3cb9a8659e6feb873bc8e17fee690
greamko <greamko@gmail.com>
2014-05-15 22:49:51 +0400

    upload2

---
+++ watch.model/SqlRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Ninject;
+
+namespace watch.model
+{
+    public partial class SqlRepository : IRepository
+    {
+        [Inject]
+        public watchDbDataContext Db { get; set; }
+    }
+}
---
+++ watch.model/SqlRepository/User.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace watch.model
+{
+    public partial class SqlRepository
+    {
+
+
+        public IQueryable<User> Users
+        {
+            get
+            {
+                return Db.Users;
+            }
+        }
+
+        public bool CreateUser(User instance)
+        {
+            if (instance.Id == 0)
+            {
+                Db.Users.InsertOnSubmit(instance);
+                Db.Users.Context.SubmitChanges();
+                return true;
+            }
+
+            return false;
+        }
+
+        public bool UpdateUser(User instance)
+        {
+            User cache = Db.Users.Where(p => p.Id == instance.Id).FirstOrDefault();
+            if (cache != null)
+            {
+
+                cache.Email = instance.Email;
+                Db.Users.Context.SubmitChanges();
+                return true;
+            }
+
+            return false;
+        }
+
+        public bool RemoveUser(int idUser)
+        {
+            User instance = Db.Users.Where(p => p.Id == idUser).FirstOrDefault();
+            if (instance != null)
+            {
+                Db.Users.DeleteOnSubmit(instance);
+                Db.Users.Context.SubmitChanges();
+                return true;
+            }
+
+            return false;
+        }
+
+    }
+}
